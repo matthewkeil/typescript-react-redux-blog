7c615f0afecc798ef4ea0f2feef8fc66639ab10c
Matthew Keil <malibuss67@gmail.com>
2018-05-23 11:57:46 +0100

    removed server/.build from .git

--- server/.build/server/src/app.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var chai_1 = require("chai");
-describe('app', function () {
-    it('should work', function () {
-        chai_1.expect(true).to.be.true;
-    });
-});
-//# sourceMappingURL=app.spec.js.map
